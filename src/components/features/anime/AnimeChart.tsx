93b2f74ce91b6962be10baf3f9292e83c63e17fb
andrewdev <developerdev370@gmail.com>
2025-05-06 15:08:46 +0400

    fix: added return for useMemo

--- src/components/features/anime/AnimeChart.tsx
+++ src/components/features/anime/AnimeChart.tsx
@@ -37,7 +37,7 @@ const AnimeChart: React.FC<AnimeChartProps> = ({
   onColumnClick,
 }) => {
   const chartData = useMemo(() => {
-    mediaList
+    return mediaList
       .filter((media): media is Media => media?.averageScore !== null)
       .map((media) => ({
         id: media.id,
